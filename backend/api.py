4c618d440634f86f368d8c0aa91bf570d977c2cc
oscarduon <oscarduon@gmail.com>
2026-09-26 00:06:37 +0200

    Fix python indentation error in DTM preview generation

--- backend/api.py
+++ backend/api.py
@@ -221,25 +221,23 @@ async def execute_command(command: str = Form(...)):
             file_info = {"name": filename, "type": ext, "url": f"/files/{filename}"}
             
             if ext == "dtm":
-                  out_tif = f + ".tif"
-                  out_jpg = f + ".jpg"
-                  # Convert DTM to TIF using FUSION
-                  if is_windows:
-                      subprocess.run(f'C:\\FUSION\\DTM2TIF.exe "{f}" "{out_tif}"', shell=True)
-                      if os.path.exists(out_tif):
-                          subprocess.run(f'python backend/tif_preview.py "{out_tif}" "{out_jpg}"', shell=True)
-                  else:
-                      subprocess.run(f"xvfb-run -a wine ~/.wine/drive_c/FUSION/DTM2TIF.exe '{f}' '{out_tif}'", shell=True)
-                      if os.path.exists(out_tif):
-                          subprocess.run(f"/home/oscar/fusion_web/venv/bin/python3 /home/oscar/fusion_web/backend/tif_preview.py '{out_tif}' '{out_jpg}'", shell=True)
-                          
-                  if os.path.exists(out_jpg):
-                      with open(out_jpg, "rb") as img_f:
-                          file_info["preview_b64"] = base64.b64encode(img_f.read()).decode('utf-8')
-                      os.remove(out_jpg)
-                  if os.path.exists(out_tif):
-                      os.remove(out_tif)
-              if ext in ["tif", "tiff"]:
+                out_tif = f + ".tif"
+                out_jpg = f + ".jpg"
+                if is_windows:
+                    subprocess.run(f'C:\\FUSION\\DTM2TIF.exe "{f}" "{out_tif}"', shell=True)
+                    if os.path.exists(out_tif):
+                        subprocess.run(f'python backend/tif_preview.py "{out_tif}" "{out_jpg}"', shell=True)
+                else:
+                    subprocess.run(f"xvfb-run -a wine ~/.wine/drive_c/FUSION/DTM2TIF.exe '{f}' '{out_tif}'", shell=True)
+                    if os.path.exists(out_tif):
+                        subprocess.run(f"/home/oscar/fusion_web/venv/bin/python3 /home/oscar/fusion_web/backend/tif_preview.py '{out_tif}' '{out_jpg}'", shell=True)
+                if os.path.exists(out_jpg):
+                    with open(out_jpg, "rb") as img_f:
+                        file_info["preview_b64"] = base64.b64encode(img_f.read()).decode('utf-8')
+                    os.remove(out_jpg)
+                if os.path.exists(out_tif):
+                    os.remove(out_tif)
+            if ext in ["tif", "tiff"]:
                 out_jpg = f + ".jpg"
                 if is_windows:
                     subprocess.run(f'python backend/tif_preview.py "{f}" "{out_jpg}"', shell=True)
@@ -249,7 +247,6 @@ async def execute_command(command: str = Form(...)):
                     with open(out_jpg, "rb") as img_f:
                         file_info["preview_b64"] = base64.b64encode(img_f.read()).decode('utf-8')
                     os.remove(out_jpg)
-                    
             if ext in ["las", "laz"]:
                 out_jpg = f + ".jpg"
                 # 1. Gen 2D Thumbnail
